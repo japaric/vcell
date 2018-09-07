890bb9be2f2bc5c7fe97b9a3bace09e433a7641c
Bryan Kadzban <bkadzban@google.com>
2018-09-07 10:26:03 -0700

    Add an invert_bits function, using the XOR range

--- src/lib.rs
+++ src/lib.rs
@@ -116,6 +116,27 @@ impl<T> VolatileCell<T> {
         }
     }
 
+    /// Inverts a collection of bits of the contained value with the bit-manipulation-engine, if
+    /// enabled.
+    /// See [NXP documentation] on the BME. This is an "XOR" operation.
+    ///
+    /// [NXP documentation]: https://www.nxp.com/docs/en/application-note/AN4838.pdf
+    #[inline(always)]
+    #[cfg(feature = "bit-manipulation")]
+    pub fn invert_bits(&self, bits_to_invert: T)
+        where T: Copy
+    {
+        unsafe {
+            let addr = self.value.get() as usize as u32;
+            if addr & 0xe00fffff != addr {
+                panic!("Tried to use BME on address 0x{:x?}, which is not in either the peripheral or upper-SRAM address range");
+            }
+            let bfi_addr = addr | 0x0c000000;
+            let bfi_ptr = bfi_addr as usize as *mut T;
+            ptr::write_volatile(bfi_ptr, bits_to_invert)
+        }
+    }
+
     /// See [ARM documentation] and [ST documentation] on bit-banding.
     ///
     /// [ARM documentation]: http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0337h/Behcjiic.html
