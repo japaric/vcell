dd70ce96c679c021e5ab44b37da00e7a70a86d5a
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2019-07-12 15:39:47 +0300

    register access traits

--- src/lib.rs
+++ src/lib.rs
@@ -59,3 +59,143 @@ impl<T> VolatileCell<T> {
 
 // NOTE implicit because of `UnsafeCell`
 // unsafe impl<T> !Sync for VolatileCell<T> {}
+
+/// Reset value of the register
+pub trait ResetValue {
+    /// Reset value of the register
+    const RESET_VALUE: Self;
+}
+
+/// Reads the contents of the register
+pub trait ReadRegister<R, T>: core::ops::Deref<Target = VolatileCell<T>>
+where
+    T: Copy + Into<R>,
+{
+    /// Reads the contents of the register
+    #[inline]
+    fn read(&self) -> R {
+        (*self).get().into()
+    }
+}
+
+/// Writes to the register using `RESET_VALUE` as basis
+pub trait WriteRegisterWithReset<W, T>: core::ops::Deref<Target = VolatileCell<T>>
+where
+    W: ResetValue + core::ops::Deref<Target = T>,
+    T: Copy,
+{
+    /// Writes to the register
+    #[inline]
+    fn write<F>(&self, f: F)
+    where
+        F: FnOnce(&mut W) -> &mut W,
+    {
+        let mut w = W::RESET_VALUE;
+        f(&mut w);
+        (*self).set(*w);
+    }
+}
+
+/// Writes to the register
+pub trait WriteRegisterWithZero<W, T>: core::ops::Deref<Target = VolatileCell<T>>
+where
+    W: core::ops::Deref<Target = T>,
+    T: Copy + Default + Into<W>,
+{
+    /// Writes to the register
+    #[inline]
+    fn write<F>(&self, f: F)
+    where
+        F: FnOnce(&mut W) -> &mut W,
+    {
+        let mut w = T::default().into();
+        f(&mut w);
+        (*self).set(*w);
+    }
+}
+
+/// Writes the reset value to the register
+pub trait ResetRegister<W, T>: WriteRegisterWithReset<W, T>
+where
+    W: ResetValue + core::ops::Deref<Target = T>,
+    T: Copy,
+{
+    /// Writes the reset value to the register
+    #[inline]
+    fn reset(&self) {
+        self.write(|w| w)
+    }
+}
+
+/// Writes Zero to the register
+pub trait ResetRegisterWithZero<W, T>: WriteRegisterWithZero<W, T>
+where
+    W: core::ops::Deref<Target = T>,
+    T: Copy + Default + Into<W>,
+{
+    /// Writes Zero to the register
+    #[inline]
+    fn reset(&self) {
+        self.write(|w| w)
+    }
+}
+
+/// Modifies the contents of the register
+pub trait ModifyRegister<R, W, T>: core::ops::Deref<Target = VolatileCell<T>>
+where
+    W:  core::ops::Deref<Target = T>,
+    T: Copy + Into<R> + Into<W>,
+{
+    /// Modifies the contents of the register
+    #[inline]
+    fn modify<F>(&self, f: F)
+    where
+        for<'w> F: FnOnce(&R, &'w mut W) -> &'w mut W,
+    {
+        let bits = (*self).get();
+        let r: R = bits.into();
+        let mut w: W = bits.into();
+        f(&r, &mut w);
+        (*self).set(*w);
+    }
+}
+
+/// Single bit read access proxy
+pub trait BitR {
+    /// Returns `true` if the bit is clear (0)
+    #[inline]
+    fn bit_is_clear(&self) -> bool {
+        !self.bit()
+    }
+    /// Returns `true` if the bit is set (1)
+    #[inline]
+    fn bit_is_set(&self) -> bool {
+        self.bit()
+    }
+    /// Returns the current state of the bit as boolean
+    fn bit(&self) -> bool;
+}
+
+/// Single bit write access proxy
+pub trait BitW<'a, W> {
+    /// Sets the field bit
+    #[inline]
+    fn set_bit(self) -> &'a mut W
+    where
+        Self: core::marker::Sized,
+    {
+        self.bit(true)
+    }
+    /// Clears the field bit
+    #[inline]
+    fn clear_bit(self) -> &'a mut W
+    where
+        Self: core::marker::Sized,
+    {
+        self.bit(false)
+    }
+    /// Writes raw bit(s) to the field
+    fn bit(self, value: bool) -> &'a mut W
+    where
+        Self: core::marker::Sized;
+}
